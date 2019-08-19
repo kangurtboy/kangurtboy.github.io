2f50c9b4dca2789e3413869a7d6c9a6a589bb307
kangurtboy <kangurt.boy@gmail.com>
2019-08-20 03:54:26 +0500

    Add files via upload

---
+++ test-project-1/source/script/main.js
@@ -0,0 +1,13 @@
+var mainNav   =    document.querySelector('.main-nav');
+var toggle    =    document.querySelector('.main-nav__toggle');
+
+mainNav.classList.remove('main-nav--nojs');
+toggle.addEventListener('click' , function() {
+	if (mainNav.classList.contains('main-nav--closed')){
+		mainNav.classList.remove('main-nav--closed');
+		mainNav.classList.add('main-nav--opened');
+	}else{
+		mainNav.classList.add('main-nav--closed');
+		mainNav.classList.remove('main-nav--opened');
+	}
+});
