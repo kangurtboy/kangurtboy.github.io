de5ff7e61c3de2097b083d1f2b782e34d513de91
o_ebrahim <kangurt.boy@gmail.com>
2022-07-14 16:45:42 +0500

    Написал скрипт для модалного окна

--- test-project-1/source/script/main.js
+++ test-project-1/source/script/main.js
@@ -1,4 +1,5 @@
-var mainNav   =    document.querySelector('.main-nav');
+//открытие и закрытие гамбургер меню
+var mainNav = document.querySelector('.main-nav');
 var toggle    =    document.querySelector('.main-nav__toggle');
 
 mainNav.classList.remove('main-nav--nojs');
@@ -11,3 +12,17 @@ toggle.addEventListener('click' , function() {
 		mainNav.classList.remove('main-nav--opened');
 	}
 });
+
+//модалное окно
+var modalEl = document.querySelector('.modal');
+var loginEl = document.querySelector('.user-list__link');
+var closeButton = document.querySelector('.modal__close');
+
+document.addEventListener('click', function (event) {
+	if (event.target === loginEl || event.target.classList.contains('user-list__login-icon')) {
+		modalEl.classList.add('modal__open');
+	}
+	if (event.target === closeButton) {
+		modalEl.classList.remove('modal__open');	
+	}
+})
