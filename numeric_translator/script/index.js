fd2629b0980e4495416de7815528f4e2388a4e0e
o_ebrahim <kangurt.boy@gmail.com>
2023-04-07 17:20:52 +0500

    Реализация основного функция

--- numeric_translator/script/index.js
+++ numeric_translator/script/index.js
@@ -1,38 +1,61 @@
 const tjAlphabet = {
-  1: 'а',
-  2: 'б',
-  3: 'в',
-  4: 'г',
-  5: 'ғ',
-  6: 'д',
-  7: 'е',
-  8: 'ё',
-  9: 'ж',
-  10: 'з',
-  11: 'и',
-  12: 'й',
-  13: 'ӣ',
-  14: 'к',
-  15: 'қ',
-  16: 'л',
-  17: 'м',
-  18: 'н',
-  19: 'о',
-  20: 'п',
-  21: 'р',
-  22: 'с',
-  23: 'т',
-  24: 'у',
-  25: 'ӯ',
-  26: 'ф',
-  27: 'х',
-  28: 'ҳ',
-  29: 'ч',
-  30: 'ҷ',
-  31: 'ш',
-  32: 'ъ',
-  33: 'э',
-  34: 'ю',
-  35: 'я',
+  а: 1,
+  б: 2,
+  в: 3,
+  г: 4,
+  ғ: 5,
+  д: 6,
+  е: 7,
+  ё: 8,
+  ж: 9,
+  з: 10,
+  и: 11,
+  й: 12,
+  ӣ: 13,
+  к: 14,
+  қ: 15,
+  л: 16,
+  м: 17,
+  н: 18,
+  о: 19,
+  п: 20,
+  р: 21,
+  с: 22,
+  т: 23,
+  у: 24,
+  ӯ: 25,
+  ф: 26,
+  х: 27,
+  ҳ: 28,
+  ч: 29,
+  ҷ: 30,
+  ш: 31,
+  ъ: 32,
+  э: 33,
+  ю: 34,
+  я: 35,
 };
 
+const inputEl = document.querySelector('.text_input'),
+  translateButton = document.querySelector('.translate_button'),
+  resultEl = document.querySelector('.result_text');
+
+const handleTranslate = (evt) => {
+  const value = inputEl.value;
+  const valueArr = value.split('');
+  const result = [];
+
+  valueArr.forEach((element) => {
+    const letter = tjAlphabet[element.toLowerCase() || element];
+
+    if (letter) {
+      result.push(`${letter}.`);
+    } else {
+      result.push(element);
+    }
+  });
+
+  resultEl.textContent = result.join('');
+};
+
+translateButton.addEventListener('click', handleTranslate);
